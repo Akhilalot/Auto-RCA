ac4aa4c3e87ac882b94c00165e44ee124733bcdf
Akhil-Philips-genAi <akhil.sambaraju@philips.com>
2026-03-27 16:04:32 +0530

    updated test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
-
+url="http://testignore-fredhca8d8erfhdc.centralindia-01.azurewebsites.net/"
+#url="http://localhost:8000/"
 response = requests.post(
-    "http://localhost:8000/run-agent",
+    f"{url}run-agent",
     json={
         "time_stamp": "2026-03-25T10:30:00Z",
         "issue": "High API latency detected on the payment service. Response times have increased from 200ms to over 2s in the last 30 minutes."
